cc3f62322b48b14b6841a340582f76d62743e4eb
garni0000 <eurahttps.gg@gmail.com>
2025-03-11 14:06:22 +0200

    Create MainActivity.kt

---
+++ app/src/main/java/com/example/crashpredictor/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.crashpredictor
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+        webView = findViewById(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("file:///android_asset/index.html")
+    }
+    
+    override fun onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
